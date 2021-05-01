9ced67a5b45c2a17c67a16bab84223aaae2de29b
iamgaddam96 <gaddamsaikiran1995@gmail.com>
2021-05-01 18:39:45 +0530

    maven project

---
+++ venkatarao/src/main/java/ramukumari/App.java
@@ -0,0 +1,13 @@
+package ramukumari;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
